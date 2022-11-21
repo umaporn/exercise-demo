725bf3265429a030d56739dc7a1d0ae6937d1475
Umaporn D <umaporn.don@gmail.com>
2022-11-21 16:57:04 +0700

    Modified test case.

--- tests/Feature/ContactControllerTest.php
+++ tests/Feature/ContactControllerTest.php
@@ -16,39 +16,49 @@ class ContactControllerTest extends TestCase
     /** @test */
     public function it_should_create_inquiries_successfully(): void
     {
-        $response = $this->post( '/contact', [
+        $response = $this->post('/contact', [
             'name'    => 'john',
             'email'   => 'john@gmail.com',
             'phone'   => '123456789',
             'message' => 'greeting text',
-        ] );
+            'start_date' => '2022-11-20',
+            'end_date' => '2022-11-21',
+            'need_on_site_service' => true,
+            'address' => '2828 Walker Field Dr,	Grand Junction',
+        ]);
 
         $response->assertOk();
 
-        $this->assertDatabaseHas( 'inquiries', [
+        $this->assertDatabaseHas('inquiries', [
             'name'    => 'john',
             'email'   => 'john@gmail.com',
             'phone'   => '123456789',
             'message' => 'greeting text',
-        ] );
+            'start_date' => '2022-11-20',
+            'end_date' => '2022-11-21',
+            'need_on_site_service' => true,
+            'address' => '2828 Walker Field Dr,	Grand Junction',
+        ]);
     }
 
     public function test_inquiries_notification_is_sent(): void
     {
         Notification::fake();
 
-        $response = $this->post( '/contact', [
+        $response = $this->post('/contact', [
             'name'    => 'umaporn',
             'email'   => 'umaporn.don@gmail.com',
             'phone'   => '123456789',
             'message' => 'greeting text',
-        ] );
+            'start_date' => '2022-11-20',
+            'end_date' => '2022-11-21',
+            'need_on_site_service' => true,
+            'address' => '2828 Walker Field Dr,	Grand Junction',
+        ]);
 
         Notification::assertSentTo(
-            [ Inquiries::where( ['email' => 'umaporn.don@gmail.com'] )->first() ],
+            [Inquiries::where(['email' => 'umaporn.don@gmail.com'])->first()],
             InquiriesNotifications::class
         );
-
     }
-
 }
